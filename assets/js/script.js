68958e0b6e9a441736a48aa9021eecf86f986f87
Exousia KASEKA <kasekaexousia@gmail.com>
2024-05-31 07:04:22 +0100

    turn pages when click next or prev button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 //turn pages when click next or prev button
+const pagesTurnBtn = document.querySelectorAll('.nextprev-btn');
+pagesTurnBtn.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        const pageTurnId = element.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+        } else {
+            pageTurn.classList.add('turn');            
+        }
+    });
+});
 
 //contact me button when click
 
